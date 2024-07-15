3e2b6d450ca826b22d633f860edde1b418ea5030
Chido100 <chido_dozie@hotmail.com>
2024-07-15 11:37:50 +0100

    Initial commit

---
+++ ng_square/settings/local.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-0sxlmjcv(^4)ittln!wu!y72kvhd@41$=1@9ta4sqthzxy3)ok"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
